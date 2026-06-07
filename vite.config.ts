2d4ebabc6f010c509398600f56edd67cc2906913
Bernardo Wobeto <bernardomw@usp.br>
2026-06-07 15:11:53 -0300

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/financial-tracking/',
+  base: '/fin_app/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
